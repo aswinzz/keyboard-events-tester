645bfa10ac38a8152d630197eb7095719c95bf59
Aswin VB <aswinvb.aswin6@gmail.com>
2025-05-28 22:37:26 +0530

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 
 const outfit = Outfit({
@@ -132,6 +133,7 @@ export default function RootLayout({
         className={`${outfit.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
